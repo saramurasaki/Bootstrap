9fe6dc14572d6c4794eaad663b7a2b9c3380ae9a
村崎　沙羅 <s.murasaki0415@gmail.com>
2017-04-24 01:50:25 +0000

    acc column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170424012549) do
+ActiveRecord::Schema.define(version: 20170424014342) do
 
   create_table "messages", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "content"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20170424012549) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "title"
+    t.string   "status"
   end
 
 end
