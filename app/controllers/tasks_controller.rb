a5bb3af2b2bb21bfd519376968309781c029ed0d
村崎　沙羅 <s.murasaki0415@gmail.com>
2017-04-24 02:30:54 +0000

    acc column

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,11 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
   def index
     @tasks = Task.all
   end
 
   def show
-    set_task
   end
 
   def new
@@ -24,12 +25,9 @@ class TasksController < ApplicationController
   end
 
   def edit
-    set_task
   end
 
   def update
-    set_task
-
     if @task.update(task_params)
       flash[:success] = 'Task は正常に更新されました'
       redirect_to @task
@@ -40,7 +38,6 @@ class TasksController < ApplicationController
   end
 
   def destroy
-    set_task
     @task.destroy
 
     flash[:success] = 'Task は正常に削除されました'
@@ -49,11 +46,9 @@ class TasksController < ApplicationController
   
   private
   
-  
   def set_task
     @task = Task.find(params[:id])
   end
-  
   
   def task_params
     params.require(:task).permit(:content, :status)
