07ac9c6b997000c7ab3a68ed5557223ac96899fe
村崎　沙羅 <s.murasaki0415@gmail.com>
2017-04-25 01:51:00 +0000

    bootstrap design

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,7 @@
 Task.create(status: 'test task 1', content: 'test content 1')
 Task.create(status: 'test task 2', content: 'test content 2')
 Task.create(status: 'test task 3', content: 'test content 3')
+
+(1..100).each do |number|
+  Task.create(status: 'test title ' + number.to_s, content: 'test content ' + number.to_s)
+end
